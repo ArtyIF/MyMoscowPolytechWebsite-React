e6502d453891f1c5af55c20d750d4bfcc45c4bf5
Artyom Fomin <artyomisflash@mail.ru>
2021-03-23 23:24:02 +0300

    Переструктурировал бредкрамбы

--- client/src/components.js
+++ client/src/components.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Breadcrumbs, BreadcrumbsItem } from 'react-breadcrumbs-dynamic';
 import { Route, Switch, Link, NavLink, useLocation } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from "react-transition-group";
+import PageNames from './page-names';
 
-export function App() {
+function App() {
     return (
         <InternalApp />
     );
@@ -16,6 +17,7 @@ function InternalApp() {
         <div>
             <header>
                 <h1>Сайт Артёма Фомина</h1>
+                <BreadcrumbsItem to="/">{PageNames.Home}</BreadcrumbsItem>
                 <Breadcrumbs
                     separator={<span> &gt; </span>}
                     item={NavLink}
@@ -25,9 +27,9 @@ function InternalApp() {
             <TransitionGroup>
                 <CSSTransition key={location.key} classNames="fade" timeout={300}>
                     <Switch location={location}>
-                        <Route exact path="/" children={Home}/>
-                        <Route path="/test" children={Test}/>
-                        <Route path="*" children={Error404}/>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/test" component={Test}/>
+                        <Route render={Error404}/>
                     </Switch>
                 </CSSTransition>
             </TransitionGroup>
@@ -35,34 +37,52 @@ function InternalApp() {
     );
 }
 
-export function Home() {
+function Home() {
     return (
         <main>
-            <BreadcrumbsItem to="/">test page 1</BreadcrumbsItem>
             <p>test page 1 contents</p>
             <Link to="/test">test page 2 link</Link>
         </main>
     );
 }
 
-export function Test() {
+function Test() {
     return (
         <main>
-            <BreadcrumbsItem to="/">test page 1</BreadcrumbsItem>
-            <BreadcrumbsItem to="/test">test page 2</BreadcrumbsItem>
-            <p>test page 2 contents</p>
+            <BreadcrumbsItem to="/test">{PageNames.Test}</BreadcrumbsItem>
+            <Switch>
+                <Route path="/test/sub" component={SubTest}/>
+                <Route render={DefaultTest}/>
+            </Switch>
         </main>
     );
 }
 
-export function Error404() {
+function DefaultTest() {
+    return (
+        <div>
+            <p>test page 2 contents</p>
+            <Link to="/test/sub">test page 3 link</Link>
+        </div>
+    );
+}
+
+function SubTest() {
     return (
         <main>
-            <BreadcrumbsItem to="/">Главная страница</BreadcrumbsItem>
-            <BreadcrumbsItem to="/404">Страница не найдена</BreadcrumbsItem>
+            <BreadcrumbsItem to="/test/sub">{PageNames.SubTest}</BreadcrumbsItem>
+            <p>test page 3 contents</p>
+        </main>
+    );
+}
+
+function Error404() {
+    return (
+        <main>
+            <BreadcrumbsItem to="/404">{PageNames.Error404}</BreadcrumbsItem>
             <p>Страница не найдена!</p>
         </main>
     );
 }
 
-export default App();
+export default App;
