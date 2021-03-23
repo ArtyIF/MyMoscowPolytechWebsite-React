7b99810786298e0c2f5d5efd43d577df1ce18a4c
Artyom Fomin <artyomisflash@mail.ru>
2021-03-23 16:17:54 +0300

    Убрал лишнее, добавил бредкрамбы

---
+++ client/src/components.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Breadcrumbs, BreadcrumbsItem } from 'react-breadcrumbs-dynamic';
+import { Route, NavLink } from 'react-router-dom';
+
+export function App() {
+    return (
+        <div>
+            <header>
+                <h1>Сайт Артёма Фомина</h1>
+                <Breadcrumbs
+                    separator={<b> / </b>}
+                    item={NavLink}
+                    finalItem={'b'}
+                />
+            </header>
+            <Route exact path="/" component={Home}/>
+            <Route path="/test" component={Test}/>
+        </div>
+    );
+}
+
+export function Home() {
+    return (
+        <main>
+            <BreadcrumbsItem to="/">Главная страница</BreadcrumbsItem>
+            <p>Ага-ага</p>
+            <NavLink to="/test">тык</NavLink>
+        </main>
+    );
+}
+
+export function Test() {
+    return (
+        <main>
+            <BreadcrumbsItem to="/">Главная страница</BreadcrumbsItem>
+            <BreadcrumbsItem to="/test">Тестовая страница</BreadcrumbsItem>
+            <p>Блаблабла</p>
+        </main>
+    );
+}
+
+export default App();
