ffe270415c0e262c8dcb185d3f593bead276c866
Artyom Fomin <artyomisflash@mail.ru>
2021-03-24 10:32:35 +0300

    API disciplines

--- server/app.js
+++ server/app.js
@@ -11,9 +11,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'client', 'public'))); // несобранная статика
+//app.use(express.static(path.join(__dirname, '..', 'client', 'public'))); // несобранная статика
 app.use(express.static(path.join(__dirname, '..', 'client', 'build'))); // собранная статика
-// app.use(express.static(path.join(__dirname, 'public'))); // не знаю пригодится или нет но кто знает
+app.use(express.static(path.join(__dirname, 'public'))); // лабы
 
 app.use('/api', apiRouter);
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+
+const getDirectories = source => fs.readdirSync(source, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
 
 router.get('/disciplines', function(req, res, next) {
-    res.json([{"id": "zero", "name": "Ноль"}]);
+    let disciplineIDs = getDirectories(path.join(__dirname, '..', 'public', 'labs'));
+    let disciplineDictList = [];
+    disciplineIDs.map((value) => {
+        let humanName = fs.readFileSync(path.join(__dirname, '..', 'public', 'labs', value, 'humanname'), 'utf8');
+        disciplineDictList.push({'id': value, 'name': humanName})
+    });
+    res.json(disciplineDictList);
 });
 
 module.exports = router;
