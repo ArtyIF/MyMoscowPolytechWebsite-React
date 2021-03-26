8992ca63c873c4282128f1c06740d47d4485c8a4
Artyom Fomin <artyomisflash@mail.ru>
2021-03-26 10:29:26 +0300

    Плейсхолдер во время загрузки списка

--- client/src/components/StuffList.js
+++ client/src/components/StuffList.js
@@ -4,6 +4,7 @@ import HumanName from './HumanName';
 
 class StuffList extends Component {
     state = {
+        loaded: false,
         error: null,
         response: []
     }
@@ -12,22 +13,28 @@ class StuffList extends Component {
         fetch(this.props.apiURL).then((res) => res.json())
             .then((res) => {
                 this.setState({response: res});
+                this.setState({loaded: true});
             })
             .then((err) => {
                 this.setState({error: err});
+                this.setState({loaded: true});
             })
     }
     
     render() {
         let res;
-        if (!this.error) {
-            res = this.state.response.map((value) => (
-                <div className='link'>
-                    {<Link to={this.props.pageURLPrefix + value}><HumanName apiURL={this.props.humanNameURLPrefix + value} /></Link>}
-                </div>
-            ));
+        if (this.state.loaded) {
+            if (!this.state.error) {
+                res = this.state.response.map((value) => (
+                    <div className='link'>
+                        {<Link to={this.props.pageURLPrefix + value}><HumanName apiURL={this.props.humanNameURLPrefix + value} /></Link>}
+                    </div>
+                ));
+            } else {
+                res = (<p>Ошибка загрузки: {this.state.error}</p>);
+            }
         } else {
-            res = (<p>Ошибка загрузки: {this.state.error}</p>)
+            res = (<p>Загрузка...</p>);
         }
         return (<div className='stuff-list'>{res}</div>);
     }
