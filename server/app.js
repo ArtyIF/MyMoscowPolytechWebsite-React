61dc38bc1f84c3c57ffecca0d92c52ff996850b2
Artyom Fomin <artyomisflash@mail.ru>
2021-03-28 13:47:03 +0300

    Доделал совместимость

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public'))); // лабы
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build'))); // собранная статика
 app.use('/api', apiRouter);
+
 app.use('/index.php', function(req, res) {
     res.redirect(301, '/');
 });
@@ -26,6 +27,11 @@ app.use('/view.php', function(req, res) {
 app.use('/webapp_lr3_task3.html', function(req, res) {
     res.redirect(301, '/labs/y_1/d_webapp/l_3/p_3');
 });
+app.use('/iframes', function(req, res) {
+    let addressSplit = req.url.split('/').splice(1);
+    res.redirect(301, '/api/page?year=1&discipline=' + addressSplit[0] + '&lab=' + addressSplit[1].substring(2) + '&page=' + addressSplit[2].split('.').slice(0, -1).join('.'));
+});
+
 app.use('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
 });
