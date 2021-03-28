93c6ff672dc0e0e6a2cd3a2bfa4a48eef3651579
Artyom Fomin <artyomisflash@mail.ru>
2021-03-28 13:30:39 +0300

    Обратная совместимость

--- server/app.js
+++ server/app.js
@@ -17,6 +17,15 @@ app.use(express.static(path.join(__dirname, 'public'))); // лабы
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build'))); // собранная статика
 app.use('/api', apiRouter);
+app.use('/index.php', function(req, res) {
+    res.redirect(301, '/');
+});
+app.use('/view.php', function(req, res) {
+    res.redirect(301, '/labs/y_1/d_' + req.query.disc + '/l_' + req.query.lr + '/p_' + req.query.sect);
+});
+app.use('/webapp_lr3_task3.html', function(req, res) {
+    res.redirect(301, '/labs/y_1/d_webapp/l_3/p_3');
+});
 app.use('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
 });
