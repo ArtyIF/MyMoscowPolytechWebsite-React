b51550187cfcde1d51016ad074cf2c76b1628a2d
Artyom Fomin <artyomisflash@mail.ru>
2021-03-28 12:51:18 +0300

    Пока нерабочая кнопка для просмотра кода

--- client/src/components/LabPageView.js
+++ client/src/components/LabPageView.js
@@ -54,16 +54,15 @@ class LabPageView extends Component {
             <div className='lab-view-main height-100'>
                 <BreadcrumbsItem to={'/labs/' + year + '/' + discipline + '/' + year}><HumanName apiURL={'/api/humanname?year=' + yearID + '&discipline=' + disciplineID + '&lab=' + labID} /></BreadcrumbsItem>
                 <div className='lab-navbar'>
-                    <span>
-                        <span>Страницы: </span>
-                        {this.state.availablePages.map((value) => {
-                            if (pageID !== value) {
-                                return (<Link to={'/labs/' + year + '/' + discipline + '/' + lab + '/p_' + value}>{value}</Link>);
-                            } else {
-                                return (<b>{value}</b>);
-                            }
-                        })}
-                    </span>
+                    <span>Страницы: </span>
+                    {this.state.availablePages.map((value) => {
+                        if (pageID !== value) {
+                            return (<Link to={'/labs/' + year + '/' + discipline + '/' + lab + '/p_' + value}>{value}</Link>);
+                        } else {
+                            return (<b>{value}</b>);
+                        }
+                    })}
+                    <a href='#' className='code-button'>Код</a>
                 </div>
                 <iframe srcDoc={this.state.sentPage} />
             </div>
